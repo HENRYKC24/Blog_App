c989ae5fb18390dec58357878c6c5f0fd131dce3
HENRY-KC <henrykc24@yahoo.com>
2022-01-18 13:10:47 +0100

    Add required methods in the models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_comments_counter
+    Comment.find_by(:id :id).count.update
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+
+  def update_posts_counter
+    Post.find_by(:id :id).count.update
+  end
+
+  def last_5_comments
+    Comment.last(5)
+  end
 end
