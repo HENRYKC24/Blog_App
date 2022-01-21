f32341a525dd419df68f5ac4129d545622077212
HENRY-KC <henrykc24@yahoo.com>
2022-01-21 15:26:00 +0100

    Add links to the user section

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def recent_posts
+  def last_recent_posts
     posts.limit(3).order(created_at: :desc)
   end
 end
