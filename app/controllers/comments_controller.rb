3fb7f84599f6558c729282ef0c429b82a6ce25b6
HENRY-KC <henrykc24@yahoo.com>
2022-01-27 21:11:50 +0100

    Add likes and comments controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(text: comment_params[:text], author_id: current_user.id, post_id: @post.id)
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          flash[:success] = 'You have successfully created a comment.'
+          redirect_to user_post_path(@post.author.id, @post.id)
+        else
+          flash.now[:error] = 'Error: Comment could not be saved'
+          redirect_to user_post_path(@post.author.id, @post.id)
+        end
+      end
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:title, :text)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.create!(author_id: current_user.id, post_id: @post.id)
+    respond_to do |format|
+      format.html do
+        if @like.save
+          flash[:success] = 'You have successfully liked this post.'
+          redirect_to user_post_path(@post.author.id, @post.id)
+        else
+          flash.now[:error] = 'Error: like could not be saved'
+          redirect_to user_post_path(@post.author.id, @post.id)
+        end
+      end
+    end
+  end
 end
