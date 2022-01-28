61da1527f40d26ccc2f0dc790c8540d81c354464
HENRY-KC <henrykc24@yahoo.com>
2022-01-28 18:58:27 +0100

    Validate model attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.limit(5).order(created_at: :desc)
   end
