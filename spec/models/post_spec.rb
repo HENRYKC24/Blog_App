3eecff577a8d41bd0f242644b564829e4f625b5d
HENRY-KC <henrykc24@yahoo.com>
2022-01-28 19:00:29 +0100

    Write tests for the models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validaiton for post' do
+    subject do
+      henry = User.new(name: 'Henry Kc', photo: 'profile.png', bio: "I am coming out to be the best computer programmer", posts_counter: 0)
+      Post.new(title: 'My best friend', text: 'He will be unvailed at the end of my program at Microverse', comments_counter: 3, likes_counter: 3, author_id: henry.id)
+    end
+
+    before { subject.save }
+
+    it 'Should have title present' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have title not being too long' do
+      subject.title = 'one' * 200
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have comments counter as an integer' do
+      subject.comments_counter = 0.033
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have comments counter not be less than zero' do
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have likes counter not be less than zero' do
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
 end
