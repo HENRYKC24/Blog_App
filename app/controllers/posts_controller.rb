743cdbcd534bc8e720077ad04ea6461de208acd1
HENRY-KC <henrykc24@yahoo.com>
2022-01-28 18:57:45 +0100

    Update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.includes(:posts).find(params[:user_id])
     @posts = @user.posts.order(created_at: :desc)
   end
 
@@ -16,6 +16,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counter = 0
     respond_to do |format|
       format.html do
         if @post.save
