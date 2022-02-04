6814030a2f73c3d28d6297fb4957544c47c27235
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-02-04 07:19:24 +0100

    Add Admin role to ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,14 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-
+    user ||= User.new # guest user (not logged in)
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
+      can :read, :all
+    end
     
     #
     # The first argument to `can` is the action you are giving the user
